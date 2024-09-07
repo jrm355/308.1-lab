d9ef66f147973cfff2bb970eeed32d0cf965cc0d
jrm355 <jeremykazdan34@gmail.com>
2024-09-06 20:41:45 -0400

    kind of a mess

---
+++ lab2.js
@@ -0,0 +1,47 @@
+//part two
+// defining values
+
+const td = 1500;
+const mph1 = 55
+const mph2 = 60
+const mph3 = 75
+const mpg1 = 30
+const mpg2 = 28
+const mpg3 = 23
+const fb = 175
+const cpg = 3
+
+//55
+
+// gallons needed
+td / mpg1
+
+// traveling at 55 mph cost
+const cost1 = (td / mpg1) <175
+Constant.log (cost)   
+
+// time traveling
+const traveltime1 = (td/mph1)
+
+//60
+// gallons needed
+td/mpg2= totalgallonsneeded1
+
+// traveling at 60 mph cost
+const cost2 = (td / mpg2) <175
+Constant.log (cost)   
+
+// time traveling
+const traveltime2 = (td/mph1)
+
+//75
+// gallons needed
+td/mpg3
+
+// traveling at 75 mph cost
+const cost3 = (td / mpg3) <175
+Constant.log (cost)   
+
+// time traveling
+const traveltime3 = (td/mph3)
+
